8a11fc2725f805b979a87e7585d7a69ae62ebf52
S M Hasibur Rahman <hasibrahman1997@gmail.com>
2020-06-25 19:53:49 -0400

    comment added

---
+++ Lab 3/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.inputTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.unitsShowTextBox = New System.Windows.Forms.TextBox()
+        Me.days = New System.Windows.Forms.Label()
+        Me.averageE1Textbox = New System.Windows.Forms.TextBox()
+        Me.totalAverageTextBox = New System.Windows.Forms.TextBox()
+        Me.enterButton = New System.Windows.Forms.Button()
+        Me.unitsShowTextBox2 = New System.Windows.Forms.TextBox()
+        Me.averageE2Textbox = New System.Windows.Forms.TextBox()
+        Me.unitsShowTextBox3 = New System.Windows.Forms.TextBox()
+        Me.averageE3Textbox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.errorToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.resetButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Day"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Units:"
+        '
+        'inputTextBox
+        '
+        Me.inputTextBox.Location = New System.Drawing.Point(66, 46)
+        Me.inputTextBox.Name = "inputTextBox"
+        Me.inputTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.inputTextBox.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(27, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Employee 1"
+        '
+        'unitsShowTextBox
+        '
+        Me.unitsShowTextBox.Location = New System.Drawing.Point(19, 135)
+        Me.unitsShowTextBox.Multiline = True
+        Me.unitsShowTextBox.Name = "unitsShowTextBox"
+        Me.unitsShowTextBox.ReadOnly = True
+        Me.unitsShowTextBox.Size = New System.Drawing.Size(125, 184)
+        Me.unitsShowTextBox.TabIndex = 4
+        '
+        'days
+        '
+        Me.days.AutoSize = True
+        Me.days.Location = New System.Drawing.Point(44, 13)
+        Me.days.Name = "days"
+        Me.days.Size = New System.Drawing.Size(16, 17)
+        Me.days.TabIndex = 5
+        Me.days.Text = "1"
+        '
+        'averageE1Textbox
+        '
+        Me.averageE1Textbox.Location = New System.Drawing.Point(19, 325)
+        Me.averageE1Textbox.Name = "averageE1Textbox"
+        Me.averageE1Textbox.ReadOnly = True
+        Me.averageE1Textbox.Size = New System.Drawing.Size(125, 22)
+        Me.averageE1Textbox.TabIndex = 6
+        '
+        'totalAverageTextBox
+        '
+        Me.totalAverageTextBox.Location = New System.Drawing.Point(19, 354)
+        Me.totalAverageTextBox.Name = "totalAverageTextBox"
+        Me.totalAverageTextBox.ReadOnly = True
+        Me.totalAverageTextBox.Size = New System.Drawing.Size(448, 22)
+        Me.totalAverageTextBox.TabIndex = 7
+        Me.totalAverageTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'enterButton
+        '
+        Me.enterButton.Location = New System.Drawing.Point(19, 382)
+        Me.enterButton.Name = "enterButton"
+        Me.enterButton.Size = New System.Drawing.Size(125, 33)
+        Me.enterButton.TabIndex = 8
+        Me.enterButton.Text = "Enter"
+        Me.enterButton.UseVisualStyleBackColor = True
+        '
+        'unitsShowTextBox2
+        '
+        Me.unitsShowTextBox2.Location = New System.Drawing.Point(176, 135)
+        Me.unitsShowTextBox2.Multiline = True
+        Me.unitsShowTextBox2.Name = "unitsShowTextBox2"
+        Me.unitsShowTextBox2.ReadOnly = True
+        Me.unitsShowTextBox2.Size = New System.Drawing.Size(131, 184)
+        Me.unitsShowTextBox2.TabIndex = 9
+        '
+        'averageE2Textbox
+        '
+        Me.averageE2Textbox.Location = New System.Drawing.Point(176, 326)
+        Me.averageE2Textbox.Name = "averageE2Textbox"
+        Me.averageE2Textbox.ReadOnly = True
+        Me.averageE2Textbox.Size = New System.Drawing.Size(131, 22)
+        Me.averageE2Textbox.TabIndex = 10
+        '
+        'unitsShowTextBox3
+        '
+        Me.unitsShowTextBox3.Location = New System.Drawing.Point(338, 135)
+        Me.unitsShowTextBox3.Multiline = True
+        Me.unitsShowTextBox3.Name = "unitsShowTextBox3"
+        Me.unitsShowTextBox3.ReadOnly = True
+        Me.unitsShowTextBox3.Size = New System.Drawing.Size(129, 184)
+        Me.unitsShowTextBox3.TabIndex = 11
+        '
+        'averageE3Textbox
+        '
+        Me.averageE3Textbox.Location = New System.Drawing.Point(338, 325)
+        Me.averageE3Textbox.Name = "averageE3Textbox"
+        Me.averageE3Textbox.ReadOnly = True
+        Me.averageE3Textbox.Size = New System.Drawing.Size(129, 22)
+        Me.averageE3Textbox.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(189, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 17)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Employee 2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(359, 100)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 17)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Employee 3"
+        '
+        'errorToolTip
+        '
+        Me.errorToolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.[Error]
+        '
+        'resetButton
+        '
+        Me.resetButton.Location = New System.Drawing.Point(182, 382)
+        Me.resetButton.Name = "resetButton"
+        Me.resetButton.Size = New System.Drawing.Size(125, 32)
+        Me.resetButton.TabIndex = 15
+        Me.resetButton.Text = "Reset"
+        Me.resetButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(338, 382)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(129, 31)
+        Me.exitButton.TabIndex = 16
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.enterButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 427)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.resetButton)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.averageE3Textbox)
+        Me.Controls.Add(Me.unitsShowTextBox3)
+        Me.Controls.Add(Me.averageE2Textbox)
+        Me.Controls.Add(Me.unitsShowTextBox2)
+        Me.Controls.Add(Me.enterButton)
+        Me.Controls.Add(Me.totalAverageTextBox)
+        Me.Controls.Add(Me.averageE1Textbox)
+        Me.Controls.Add(Me.days)
+        Me.Controls.Add(Me.unitsShowTextBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.inputTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Average Units Shipped By Employee "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents inputTextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents unitsShowTextBox As TextBox
+    Friend WithEvents days As Label
+    Friend WithEvents averageE1Textbox As TextBox
+    Friend WithEvents totalAverageTextBox As TextBox
+    Friend WithEvents enterButton As Button
+    Friend WithEvents unitsShowTextBox2 As TextBox
+    Friend WithEvents averageE2Textbox As TextBox
+    Friend WithEvents unitsShowTextBox3 As TextBox
+    Friend WithEvents averageE3Textbox As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents errorToolTip As ToolTip
+    Friend WithEvents resetButton As Button
+    Friend WithEvents exitButton As Button
+End Class
---
+++ Lab 3/Form1.vb
@@ -0,0 +1,166 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Dim employeeUnitsShipped(2, 6) As Integer
+    Dim index As Integer = 0
+    Dim runningTotal As Integer = 0
+    Dim averageE1 As Double
+    Dim averageE2 As Double
+    Dim averageE3 As Double
+    Dim totalAverage As Double
+    Dim outterloop As Integer = 0
+    Dim innerloop As Integer = 0
+    Dim validation As Boolean = True
+
+
+    ''' <summary>
+    ''' executes when enter button presses
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub enterButton_Click(sender As Object, e As EventArgs) Handles enterButton.Click
+        ReDim employeeUnitsShipped(2, 6)
+
+        validation = Int32.TryParse(inputTextBox.Text, employeeUnitsShipped(outterloop, innerloop))
+
+        '' If index < 6 Then
+        If validation Then
+
+            If employeeUnitsShipped(outterloop, innerloop) <= 5000 And employeeUnitsShipped(outterloop, innerloop) >= 0 Then
+
+
+                runningTotal += employeeUnitsShipped(outterloop, innerloop)
+
+
+                If outterloop <= 0 And innerloop <= 6 Then
+
+                    unitsShowTextBox.Text += employeeUnitsShipped(outterloop, innerloop).ToString + Environment.NewLine
+                    innerloop += 1
+                    index += 1
+                    days.Text = (innerloop + 1).ToString
+                    inputTextBox.Clear()
+                    If index >= 7 Then
+                        averageE1 = runningTotal / 7
+
+                        averageE1Textbox.Text = "Average is: " + Math.Round(averageE1, 2).ToString
+                        totalAverage += averageE1
+                        Label3.Font = New Font(Label3.Font, FontStyle.Regular)
+                        runningTotal = 0
+                        outterloop += 1
+                        innerloop = 0
+                        index = 0 ''reset index
+                        days.Text = (innerloop + 1).ToString
+                        Label4.Font = New Font(Label4.Font, FontStyle.Bold)
+                    End If
+
+                ElseIf outterloop <= 1 And innerloop <= 6 Then
+
+                    unitsShowTextBox2.Text += employeeUnitsShipped(outterloop, innerloop).ToString + Environment.NewLine
+                    innerloop += 1
+                    index += 1
+                    days.Text = (innerloop + 1).ToString
+                    inputTextBox.Clear()
+                    If index >= 7 Then
+                        averageE2 = runningTotal / 7
+                        averageE2Textbox.Text = "Average is: " + Math.Round(averageE2, 2).ToString
+                        totalAverage += averageE2
+                        runningTotal = 0
+                        outterloop += 1
+                        innerloop = 0
+                        index = 0
+                        days.Text = (innerloop + 1).ToString
+                        Label4.Font = New Font(Label4.Font, FontStyle.Regular)
+                        Label5.Font = New Font(Label5.Font, FontStyle.Bold)
+                    End If
+
+                ElseIf outterloop <= 2 And innerloop <= 6 Then
+                    unitsShowTextBox3.Text += employeeUnitsShipped(outterloop, innerloop).ToString + Environment.NewLine
+                    innerloop += 1
+                    index += 1
+                    days.Text = (innerloop + 1).ToString
+                    inputTextBox.Clear()
+                    If index >= 7 Then
+                        averageE3 = runningTotal / 7
+                        averageE3Textbox.Text = "Average is: " + Math.Round(averageE3, 2).ToString
+                        runningTotal = 0
+                        Label1.Text = "Done"
+                        days.Visible = False
+                        totalAverage = (averageE1 + averageE2 + averageE3) / 3
+                        totalAverageTextBox.Text = "Average per day: " + Math.Round(totalAverage, 2).ToString
+
+                        enterButton.Enabled = False
+                        inputTextBox.ReadOnly = True
+                        innerloop = 0
+
+
+
+
+                    End If
+
+
+
+
+
+
+
+
+                End If
+            Else
+                errorToolTip.Show("Please enter the number Within 0 and 5000!", inputTextBox)
+                inputTextBox.Clear()
+
+            End If
+        Else
+            errorToolTip.Show("Please Enter a valid number!", inputTextBox)
+            inputTextBox.Clear()
+        End If
+
+
+
+        '' End If
+
+
+    End Sub
+    ''' <summary>
+    ''' executes when user press exit button 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles resetButton.Click
+        inputTextBox.ReadOnly = False
+        Label1.Text = "Day"
+        days.Visible = True
+        enterButton.Enabled = True
+
+        unitsShowTextBox.Clear()
+        unitsShowTextBox2.Clear()
+        unitsShowTextBox3.Clear()
+        averageE1Textbox.Clear()
+        outterloop = 0
+        innerloop = 0
+        averageE1 = 0
+        index = 0
+        days.Text = (innerloop + 1).ToString
+        averageE2Textbox.Clear()
+        averageE2 = 0
+        averageE3Textbox.Clear()
+        averageE3 = 0
+        totalAverageTextBox.Clear()
+        totalAverage = 0
+        runningTotal = 0
+        Label4.Font = New Font(Label4.Font, FontStyle.Regular)
+        Label5.Font = New Font(Label5.Font, FontStyle.Regular)
+        Label3.Font = New Font(Label3.Font, FontStyle.Bold)
+        inputTextBox.Clear()
+
+        ''
+
+
+
+    End Sub
+End Class
